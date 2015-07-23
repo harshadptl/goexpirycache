2c67a33c287f6c3644e8a45fb73b8c004d3e331d
Harshad Patil <harshad0402@gmail.com>
2015-07-23 21:42:03 +0530

    created cache interface added goleveldb init

--- cache.go
+++ cache.go
@@ -2,12 +2,29 @@ package goexpirycache
 
 import(
 	"log",
-    "github.com/syndtr/goleveldb"
+    "github.com/syndtr/goleveldb/leveldb"
 )
 
+//Cache This interface exposes the functions which 
+//
 type Cache interface{
 	Get(key string)
     Set(key string, value *[]byte, timestamp int64, expiry int64)
     Del(key string, timestamp int64) 
     Touch(key string, expiry int64)
+}
+
+type cache struct {
+	dbName string
+    db     leveldb.DB
+}
+
+func New(dbName string) (*Cache, error) {
+	filePath := "var/" + dbName
+    db, err := leveldb.OpenFile(filePath, nil)
+    if err != nil {
+    	return nil, err
+    }
+    
+    return &cache{dbName: dbName, db: db}
 }
