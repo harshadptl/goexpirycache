17be120d5b67dfb5aa61de953db455e4b57ab460
Harshad Patil <harshad.patil@oibibo.com>
2015-07-23 21:46:23 +0530

    go fmt & minor syntax fix

--- cache.go
+++ cache.go
@@ -1,30 +1,31 @@
 package goexpirycache
 
-import(
-	"log",
-    "github.com/syndtr/goleveldb/leveldb"
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"log"
 )
 
-//Cache This interface exposes the functions which 
+//Cache This interface exposes the functions which
 //
-type Cache interface{
+type Cache interface {
 	Get(key string)
-    Set(key string, value *[]byte, timestamp int64, expiry int64)
-    Del(key string, timestamp int64) 
-    Touch(key string, expiry int64)
+	Set(key string, value *[]byte, timestamp int64, expiry int64)
+	Del(key string, timestamp int64)
+	Touch(key string, expiry int64)
 }
 
 type cache struct {
 	dbName string
-    db     leveldb.DB
+	db     *leveldb.DB
 }
 
-func New(dbName string) (*Cache, error) {
+func New(dbName string) (Cache, error) {
 	filePath := "var/" + dbName
-    db, err := leveldb.OpenFile(filePath, nil)
-    if err != nil {
-    	return nil, err
-    }
-    
-    return &cache{dbName: dbName, db: db}
+	db, err := leveldb.OpenFile(filePath, nil)
+	if err != nil {
+		log.Println("error opening leveldb file \n")
+		return nil, err
+	}
+
+	return &cache{dbName: dbName, db: db}, nil
 }
